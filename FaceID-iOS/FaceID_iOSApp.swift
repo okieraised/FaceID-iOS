78aeb793bf48c41026b14c05d8730328d1bdb9a5
okieraised <tripg@vingroup.net>
2023-06-13 09:24:11 +0700

    Initial Commit

---
+++ FaceID-iOS/FaceID_iOSApp.swift
@@ -0,0 +1,20 @@
+//
+//  FaceID_iOSApp.swift
+//  FaceID-iOS
+//
+//  Created by Tri Pham on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct FaceID_iOSApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
